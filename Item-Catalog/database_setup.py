9726ca7c18bde513e60ab4cd948651c8281dc90a
Etienne Arendse <etienne@limabean.co.za>
2017-09-11 15:57:56 +0200

    latest update

--- Item-Catalog/database_setup.py
+++ Item-Catalog/database_setup.py
@@ -10,7 +10,6 @@ class Restaurant(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    menuItem = relationship(MenuItem, backref="restaurant", passive_deletes=True)
 
     @property
     def serialize(self):
@@ -29,7 +28,7 @@ class MenuItem(Base):
     description = Column(String(250))
     price = Column(String(8))
     course = Column(String(250))
-    restaurant_id = Column(Integer,ForeignKey('restaurant.id', ondelete='CASCADE'))
+    restaurant_id = Column(Integer,ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
 
 
